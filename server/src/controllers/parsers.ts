899dae2ae9337492efa8222b7809e305369b18a3
jalexjovel <48868529+jalexjovel@users.noreply.github.com>
2025-08-14 12:08:28 -0700

    refactor(parse):feedback from pull request

--- server/src/controllers/parsers.ts
+++ server/src/controllers/parsers.ts
@@ -1,27 +1,30 @@
 import type { Request, Response, NextFunction } from 'express';
 
-function parseUserQuery(req: Request, res: Response, next: NextFunction): void {
-  console.log(req.body);
+function parseUserQuery(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response {
   if (!req.body) {
-    next({
+    return next({
       log: 'parseUserQuery: There is no request body',
       message: { err: 'Add input to the request body' },
     });
   }
   if (!req.body.userQuery) {
-    next({
+    return next({
       log: 'parseUserQuery: key "userQuery" not given',
       message: { err: 'key "userQuery" not given' },
     });
   }
   if (typeof req.body.userQuery !== 'string') {
-    next({
+    return next({
       log: 'parseUserQuery: key "userQuery" is not a string',
       message: { err: 'key "userQuery" is not a string' },
     });
   }
   res.locals.inputQuery = req.body.userQuery;
-  next();
+  return next();
 }
 
 export { parseUserQuery };
